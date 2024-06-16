1019642fbe154fd61097fe4b38caca8bce46f997
pynezz <kadam@pm.me>
2024-06-16 23:09:16 +0200

    adding fly for testing

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -1,6 +1,18 @@
 package serve
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+//go:embed templates/*
+var resources embed.FS
+
+var t = template.Must(template.ParseFS(resources, "templates/*"))
 
 func Serve(args ...string) {
 	fmt.Println("Hello from the serve package!")
@@ -11,5 +23,20 @@ func Help(args ...string) string {
 }
 
 func Execute(args ...string) {
-	fmt.Println("Hello from the serve module!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"Region": os.Getenv("FLY_REGION"),
+		}
+
+		t.ExecuteTemplate(w, "index.html.tmpl", data)
+	})
+
+	log.Println("listening on", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
 }
